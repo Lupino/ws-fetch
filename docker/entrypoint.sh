23f48d6bba4dbc533298b03c58ccdf4012fd3ae5
Lupino <lmjubuntu@gmail.com>
2019-02-21 09:05:34 +0800

    set the service on envirements

---
+++ docker/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -o errexit
+
+configfile="/config.yml"
+
+if [ ! -f "${configfile}" ]; then
+  touch ${configfile}
+  for (( i = 1; ; i++ ))
+  do
+    VAR_SERVICE="SERVICE$i"
+    VAR_HOST="HOST$i"
+
+    if [ ! -n "${!VAR_SERVICE}" ]; then
+      break
+    fi
+
+    it_service=${!VAR_SERVICE}
+    it_host=${!VAR_HOST}
+
+    cat >> ${configfile} <<_EOF_
+- service: ${it_service}
+  host: ${it_host}
+_EOF_
+  done
+fi
+
+cat ${configfile}
+
+if [ "$1" = 'ws-fetch' ]; then
+  pipeEnvironmentVariables
+  exec /usr/bin/ws-fetch ${configfile}
+fi
+
+exec "$@"
